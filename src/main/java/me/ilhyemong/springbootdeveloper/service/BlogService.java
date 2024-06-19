f3596075f70e1b6e3249592bd2dfaea1ae7a14de
dorongchu <dorong333@naver.com>
2024-06-19 17:57:03 +0900

    2024.06.19 rih - 6장까지 실습

--- src/main/java/me/ilhyemong/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/ilhyemong/springbootdeveloper/service/BlogService.java
@@ -3,8 +3,12 @@ package me.ilhyemong.springbootdeveloper.service;
 import lombok.RequiredArgsConstructor;
 import me.ilhyemong.springbootdeveloper.domain.Article;
 import me.ilhyemong.springbootdeveloper.dto.AddArticleRequest;
+import me.ilhyemong.springbootdeveloper.dto.UpdateArticleRequest;
 import me.ilhyemong.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -14,4 +18,28 @@ public class BlogService {
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
     }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(Long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+    }
+
+    public void delete(Long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(Long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+
 }
